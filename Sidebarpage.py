7b3c8a4cc19737ed56d0252e3186075738997f45
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-10 11:14:34 +0800

    Sidebarpage.py

---
+++ Sidebarpage.py
@@ -0,0 +1,15 @@
+from pickle import FALSE
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def show():
+    with st.sidebar:
+        st.markdown("""
+                    # Applications
+                    """, unsafe_allow_html = False)
+        selected = option_menu(
+            menu_title = None,
+            options = ["Text", "Image"], #required
+            default_index = 0, 
+        )
+        return selected
