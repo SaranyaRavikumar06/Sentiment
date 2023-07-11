d074e40f8c357d04e5f5f119bad5bb07100dcb3f
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 11:30:06 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -3,8 +3,9 @@ import streamlit as st
 import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
 def show():
-    st.code("import extra_streamlit_components as stx")
-    selected = stx.tab_bar(data=[
-        stx.TabBarItemData(id="Text", title="✍️ To Do", description="Tasks to take care of"),
-        stx.TabBarItemData(id="Image", title="💔 Overdue", description="Tasks missed out")])
+    with st.tabs:
+        st.code("import extra_streamlit_components as stx")
+        selected = stx.tab_bar(data=[
+            stx.TabBarItemData(id="Text", title="✍️ To Do", description="Tasks to take care of"),
+            stx.TabBarItemData(id="Image", title="💔 Overdue", description="Tasks missed out")])
 return selected
