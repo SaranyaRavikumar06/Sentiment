401a269bc5304b2b3c25629b000f3f9dfc002308
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 12:11:47 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 #import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
 def show(): 
-    bg = """<body bgcolor="#DBF9FC"><div style='background-color:black; padding:13px'>
+    bg = """<body style='background-color:"#DBF9FC"'><div style='background-color:black; padding:13px'>
               <h1 style='color:white'>Sentiment Analysis  😊😐😕😡</h1>
        </div>"""
     st.markdown(bg, unsafe_allow_html=True)
