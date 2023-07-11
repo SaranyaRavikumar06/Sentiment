d1bea5054468caaed09d0be5c40fabdc37cc18d4
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 11:21:36 +0800

    Update tabbarpage.py

--- tabbarpage.py
+++ tabbarpage.py
@@ -1,10 +1,7 @@
 from pickle import FALSE
 import streamlit as st
-import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
 def show():
     st.code("import extra_streamlit_components as stx")
-    selected = stx.tab_bar(data=[
-        stx.TabBarItemData(id="Text", title="✍️ Text Data", description="Tasks to take care of"),
-        stx.TabBarItemData(id="Image", title="💔 Image", description="Tasks missed out")])
-    return selected
+    selected = st.tabs(["Text","Image"])
+return selected
