9d289f77f5b9054f98275de03c64b4953c97f70a
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 12:10:15 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -3,22 +3,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 #import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
-"""
-def show():
-    with stx.tabs():
-        st.code("import extra_streamlit_components as stx")
-        selected = stx.tab_bar(data=[
-            stx.TabBarItemData(id="Text", title="✍️ To Do", description="Tasks to take care of"),
-            stx.TabBarItemData(id="Image", title="💔 Overdue", description="Tasks missed out"),], default=1)
-        st.info(f"{selected=}")
-        return selected
-"""
-
 def show(): 
-    bg = """<body bgcolor="#800000"><div style='background-color:black; padding:13px'>
+    bg = """<body bgcolor="#DBF9FC"><div style='background-color:black; padding:13px'>
               <h1 style='color:white'>Sentiment Analysis  😊😐😕😡</h1>
        </div>"""
     st.markdown(bg, unsafe_allow_html=True)
-    #st.title("Sentiment Analysis  😊😐😕😡")
     selected = st.radio( "👇Select the type of Sentiment Analytics ", ("Text", "Image")) 
     return selected
