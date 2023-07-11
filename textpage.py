94f098d05a286d76ebb620a68d3f5da3fd01af16
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 17:53:35 +0800

    Update textpage.py

--- textpage.py
+++ textpage.py
@@ -4,6 +4,7 @@ from textblob import TextBlob
 from PIL import Image
 import text2emotion as te
 import plotly.graph_objects as go
+import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
