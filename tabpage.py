73e62a5af436941030227c3c8cf8f9d947ac7b45
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 12:57:55 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -4,7 +4,11 @@ import streamlit.components.v1 as components
 #import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
 def show(): 
-    st.title("Sentiment Analysis  😊😐😕😡")
+    bg = """<div style='background-color:black; padding:13px'>
+              <h1 style='color:white'>Sentiment Analysis  😊😐😕😡</h1>
+       </div>"""
+    st.markdown(bg, unsafe_allow_html=True)
+   # st.title("Sentiment Analysis  😊😐😕😡")
     selected = st.radio( "👇Select the type of Sentiment Analytics ", ("Text", "Image")) 
     return selected
 def add_bg_from_url():
