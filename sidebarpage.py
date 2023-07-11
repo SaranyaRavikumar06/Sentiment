9be77e8c5ded7d77d432b7cfd3805de3e8ba7cf7
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 10:20:30 +0800

    Update sidebarpage.py

--- sidebarpage.py
+++ sidebarpage.py
@@ -2,7 +2,7 @@ from pickle import FALSE
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-def show():
+""" def show():
     with st.sidebar:
         st.markdown("""
                     # Applications
@@ -13,3 +13,13 @@ def show():
             default_index = 0, 
         )
         return selected
+"""
+def show():
+    with st.tabs():
+        selected = option_menu(
+            menu_title = None,
+            options = [ "🗃 Text","📈 Image"],
+            icons=[ "🗃 Text","📈 Image"]#required
+            default_index = 0,
+            }
+         return selected
