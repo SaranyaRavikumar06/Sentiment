893ecc58ea0732bf0c909b5063209ee8df24067e
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 16:39:55 +0800

    Update mainpagedesign.py

--- mainpagedesign.py
+++ mainpagedesign.py
@@ -2,9 +2,8 @@ import streamlit as st
 import sidebarpage
 import textpage
 import imageanalysis
-# st.title("Hello")
 page = sidebarpage.show()
 if page=="Text":
     textpage.renderPage()
-elif page=="Image":
+else:
     imageanalysis.renderPage()
