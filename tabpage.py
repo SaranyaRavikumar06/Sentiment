8c9fefe305b66b32de31fe3400c4f6f230bed5d4
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 12:30:13 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -4,14 +4,14 @@ import streamlit.components.v1 as components
 #import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
 def show(): 
-    bg = """<p style="background-color:aquamarine;"><div style='background-color:black; padding:13px'>
+    bg = """<div style='background-color:black; padding:13px'>
               <h1 style='color:white'>Sentiment Analysis  😊😐😕😡
     <style>
     .reportview-container {
-        background: url("https://www.example.com/image.jpg");
+        background: url("https://cdn-media-1.freecodecamp.org/images/1*TdiVdPnYkvgl3qWnLGgOcg.jpeg");
     }
    </style></h1>
-       </div></p>
+       </div>
     """
     
     st.markdown(bg, unsafe_allow_html=True)
