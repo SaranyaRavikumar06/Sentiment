b1856ddf57a353f7ea1b9597c2c2f17d8ab92f3e
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 11:40:42 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -1,13 +1,17 @@
 from pickle import FALSE
 import streamlit as st
-import extra_streamlit_components as stx
+#import extra_streamlit_components as stx
 from streamlit_option_menu import option_menu
+"""
 def show():
-    with st.tabs():
+    with stx.tabs():
         st.code("import extra_streamlit_components as stx")
         selected = stx.tab_bar(data=[
             stx.TabBarItemData(id="Text", title="✍️ To Do", description="Tasks to take care of"),
             stx.TabBarItemData(id="Image", title="💔 Overdue", description="Tasks missed out"),], default=1)
         st.info(f"{selected=}")
         return selected
-
+"""
+def show(): 
+    selected = st.radio( "Select a tab", ("Text", "Image")) 
+    return selected
