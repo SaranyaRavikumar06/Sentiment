cf9d6bdc1556a13d94d12e0c59f43a832a3a89e1
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 12:40:21 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -12,7 +12,8 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             #background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-color:aquamarine;
              background-attachment: fixed;
              background-size: contain;
          }}
