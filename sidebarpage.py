513e562090b466a57f0f87ff94b6d0e6e9b59975
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 13:08:06 +0800

    Update sidebarpage.py

--- sidebarpage.py
+++ sidebarpage.py
@@ -3,9 +3,18 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 def show():
     with st.sidebar:
-        st.markdown("""
-                    # Applications
-                    """, unsafe_allow_html = False)
+        st.markdown( f"""
+         <style>
+         .stApp {{
+             #background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background: content-box radial-gradient(#FF0000, white);
+             #background-color:aquamarine;
+             background-attachment: fixed;
+             background-size: contain;
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True)
         selected = option_menu(
             menu_title = None,
             options = ["Text", "Image"], #required
