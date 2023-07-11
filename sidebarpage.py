7b2ce1f0113bcb32ff9f3310a8281496d36190ee
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 13:06:47 +0800

    Update sidebarpage.py

--- sidebarpage.py
+++ sidebarpage.py
@@ -1,7 +1,7 @@
 from pickle import FALSE
 import streamlit as st
 from streamlit_option_menu import option_menu
-"""def show():
+def show():
     with st.sidebar:
         st.markdown("""
                     # Applications
@@ -12,12 +12,5 @@ from streamlit_option_menu import option_menu
             default_index = 0, 
         )
         return selected
-"""
-def show():
-    with st.tabs:
-        st.code("import extra_streamlit_components as stx")
-        selected = st.tab_bar(data=[
-            st.TabBarItemData(id="Text", title="✍️ Text Data", description="Tasks to take care of"),
-            st.TabBarItemData(id="Image", title="💔 Image", description="Tasks missed out")])
-         return selected
+
 
