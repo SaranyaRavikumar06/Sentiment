388f3db36d15fb05d762a0bddfd864c69a7c31dc
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 12:39:15 +0800

    Update tabpage.py

--- tabpage.py
+++ tabpage.py
@@ -7,3 +7,18 @@ def show():
     st.title("Sentiment Analysis  😊😐😕😡")
     selected = st.radio( "👇Select the type of Sentiment Analytics ", ("Text", "Image")) 
     return selected
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: contain;
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url() 
