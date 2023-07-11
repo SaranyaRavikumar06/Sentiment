d403caf532c02ee117ac74a79ac6496ad30d7873
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-11 13:32:40 +0800

    Update sidebarpage.py

--- sidebarpage.py
+++ sidebarpage.py
@@ -4,8 +4,12 @@ from streamlit_option_menu import option_menu
 
 def show():
     with st.sidebar:
+         bg = """<div style='background-color:white; padding:13px'>
+              <h1 style='color:black'>Sentiment Analysis  😊😐😕😡</h1>
+       </div>"""
+        st.markdown(bg, unsafe_allow_html=True)
         selected = option_menu(
-            menu_title = None,
+            menu_title = "👇Select the type of Sentiment Analytics",
             options = ["Text", "Image"], #required
             default_index = 0, 
         )
@@ -16,8 +20,8 @@ def add_bg_from_url():
          <style>
          .stApp {{
              #background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
-             background: content-box radial-gradient(#FF0000, white);
-             #background-color:aquamarine;
+             #background: content-box radial-gradient(#FF0000, white);
+             background-color:#D8BFD8;
              background-attachment: fixed;
              background-size: contain;
          }}
