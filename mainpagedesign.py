4dcc4bb476d81e5d4ade62454fd3dc64eeac4480
Saranya Ravikumar Skymind <56287965+SaranyaRavikumar06@users.noreply.github.com>
2023-07-17 10:35:17 +0800

    Update mainpagedesign.py

--- mainpagedesign.py
+++ mainpagedesign.py
@@ -4,9 +4,9 @@ import textblob
 import texttoemotion
 import vader
 page = sidebarpage.show()
-if page=="TextBlob Analysis":
+if page=="Text2Emotion Analysis":
+    texttoemotion.renderPage() 
+elif page=="TextBlob Analysis":
     textblob.input()
-elif page=="Text2Emotion Analysis":
-    texttoemotion.renderPage()
 elif page=="VADER Sentiment Analysis":
     vader.renderPage()
